1563cec56857774933dff09913c831aae2de20a0
giladhare <gilad.hare@mail.huji.ac.il>
2022-04-01 10:55:30 +0300

    Q2 and Q3 done

--- exercises/city_temperature_prediction.py
+++ exercises/city_temperature_prediction.py
@@ -14,7 +14,7 @@ COUNTRY = "Israel"
 pio.templates.default = "simple_white"
 
 
-def load_data(filename: str) -> (pd.DataFrame, pd.Series):
+def load_data(filename: str) -> (pd.DataFrame):
     """
     Load city daily temperature dataset and preprocess data.
     Parameters
@@ -32,35 +32,38 @@ def load_data(filename: str) -> (pd.DataFrame, pd.Series):
     # add day of year column
     df["DayOfYear"] = df["Date"].dt.dayofyear
 
-    response = df["Temp"]
-    return df.drop(["Temp", "Date"], axis=1), response
+    return df.drop(["Date"], axis=1)
 
 
 if __name__ == '__main__':
     np.random.seed(0)
     # Question 1 - Load and preprocessing of city temperature dataset
     data_path = "../datasets/City_Temperature.csv"
-    design_matrix, response = load_data(data_path)
+    design_matrix = load_data(data_path)
 
     # Question 2 - Exploring data for specific country
-    # scatter plot of DayOfYear and Temp
+    # scatter plot of DayOfYear and Temp colored by year
     country_df = design_matrix[design_matrix["Country"] == COUNTRY]
-    country_temp = response[country_df.index]
     string_years = country_df["Year"].astype(str)
-    fig = px.scatter(country_df, x="DayOfYear", y=country_temp, color=string_years,
+    fig = px.scatter(country_df, x="DayOfYear", y="Temp", color=string_years,
                      title="Relation between day of year and temperatue in {country}".format(country=COUNTRY),
                      labels={"DayOfYear": "Day of Year", "y": "Temperature"})
-    fig.show()
+    # fig.show()
 
+    # bar plot of std by month
     country_by_month_std = country_df.groupby('Month', as_index=False).agg('std')
     fig = px.bar(country_by_month_std, x='Month', y='Temp',
                  title="Standard deviation of temperatures by month",
                  labels={"Temp": "Std of temperatue"})
-    fig.show()
-
+    # fig.show()
 
     # Question 3 - Exploring differences between countries
-    raise NotImplementedError()
+    by_month_by_country = design_matrix.groupby(['Country', 'Month'], as_index=False
+                                                ).Temp.agg(['std', 'mean']).reset_index()
+    fig = px.line(by_month_by_country, x='Month', y='mean', error_y='std',
+                  color='Country', title="Average Temperature by Month",
+                  labels={"mean": "Average Temperature"})
+    # fig.show()
 
     # Question 4 - Fitting model for different values of `k`
     raise NotImplementedError()
