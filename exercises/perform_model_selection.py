40b53b9779124964059d49fc1e092ce32edc4889
giladhare <gilad.hare@mail.huji.ac.il>
2022-05-27 08:58:04 +0300

    ex5 - submission

--- exercises/perform_model_selection.py
+++ exercises/perform_model_selection.py
@@ -41,7 +41,7 @@ def select_polynomial_degree(n_samples: int = 100, noise: float = 5):
                      go.Scatter(x=test_X, y=test_y, mode='markers',
                                 marker=dict(color="#0000ff"), name="Test Set"),
                      ])
-    fig.update_layout(height=400, title_text="True model and test and size sets")
+    fig.update_layout(height=400, title_text=f"True model and test and size sets (noise = {noise})")
     fig.show()
 
     # Question 2 - Perform CV for polynomial fitting with degrees 0,1,...,10
@@ -51,8 +51,9 @@ def select_polynomial_degree(n_samples: int = 100, noise: float = 5):
         errors = cross_validate(PolynomialFitting(k), train_X, train_y, mean_square_error, 5)
         train_errors[k], test_errors[k] = errors
     fig = go.Figure([go.Scatter(x=k_s, y=train_errors, name="Train error"),
-                     go.Scatter(x=k_s, y=test_errors, name="Test error")])
-    fig.update_layout(title_text="Error as a function of Polynomial degree")
+                     go.Scatter(x=k_s, y=test_errors, name="Validation error")])
+    fig.update_layout(title_text=f"Error as a function of polynomial degree (noise = {noise})",
+                      xaxis_title="Polynomial degree", yaxis_title="Error Rate")
     fig.show()
 
     # Question 3 - Using best value of k, fit a k-degree polynomial model and report test error
@@ -81,33 +82,49 @@ def select_regularization_parameter(n_samples: int = 50, n_evaluations: int = 50
     test_x, test_y = X[n_samples:, :], y[n_samples:]
 
     # Question 7 - Perform CV for different values of the regularization parameter for Ridge and Lasso regressions
-    lambdas = np.linspace(0.00001, 5, n_evaluations)
-    ridge_test_errors, ridge_train_errors = np.zeros(n_evaluations), np.zeros(n_evaluations)
-    lasso_test_errors, lasso_train_errors = np.zeros(n_evaluations), np.zeros(n_evaluations)
+    lambdas = np.linspace(0.0001, 2, n_evaluations)
+    ridge_validation_errors, ridge_train_errors = np.zeros(n_evaluations), np.zeros(n_evaluations)
+    lasso_validation_errors, lasso_train_errors = np.zeros(n_evaluations), np.zeros(n_evaluations)
 
     for idx, lam in enumerate(lambdas):
-        ridge_test_errors[idx], ridge_train_errors[idx] = cross_validate(
+        ridge_train_errors[idx], ridge_validation_errors[idx] = cross_validate(
             RidgeRegression(lam), train_x, train_y, mean_square_error)
-        lasso_test_errors[idx], lasso_train_errors[idx] = cross_validate(
+        lasso_train_errors[idx], lasso_validation_errors[idx] = cross_validate(
             Lasso(lam), train_x, train_y, mean_square_error)
 
     fig = go.Figure([go.Scatter(x=lambdas, y=ridge_train_errors, name="Ridge Train"),
-                         go.Scatter(x=lambdas, y=ridge_test_errors, name="Ridge Test"),
+                         go.Scatter(x=lambdas, y=ridge_validation_errors, name="Ridge Validation"),
                          go.Scatter(x=lambdas, y=lasso_train_errors, name="Lasso Train"),
-                         go.Scatter(x=lambdas, y=lasso_test_errors, name="Lasso Test")])
+                         go.Scatter(x=lambdas, y=lasso_validation_errors, name="Lasso Validation")])
     fig.update_layout(title_text="Ridge and Lasso errors as function of lambda")
     fig.show()
+
     # Question 8 - Compare best Ridge model, best Lasso model and Least Squares model
-    raise NotImplementedError()
+    best_ridge_lambda = lambdas[np.argmin(ridge_validation_errors)]
+    best_lasso_lambda = lambdas[np.argmin(lasso_validation_errors)]
+    print(f"Best regularization parameter for ridge: {best_ridge_lambda}")
+    print(f"Best regularization parameter for lasso: {best_lasso_lambda}")
+
+    least_square_model = LinearRegression().fit(train_x, train_y)
+    ridge_model = RidgeRegression(best_ridge_lambda).fit(train_x, train_y)
+    lasso_model = Lasso(best_lasso_lambda).fit(train_x, train_y)
+
+    least_square_test_err = least_square_model.loss(test_x, test_y)
+    ridge_model_test_err = ridge_model.loss(test_x, test_y)
+    lasso_model_test_err = mean_square_error(test_y, lasso_model.predict(test_x))
+    print(f"Least Squares test error: {least_square_test_err}")
+    print(f"Ridge Model test error: {ridge_model_test_err}")
+    print(f"Lasso Model test error: {lasso_model_test_err}")
 
 
 if __name__ == '__main__':
     np.random.seed(0)
     # questions 1-3
-    # select_polynomial_degree()
+    select_polynomial_degree()
     # question 4
-    # select_polynomial_degree(noise=0)
+    select_polynomial_degree(noise=0)
     # question 5
-    # select_polynomial_degree(n_samples=1500, noise=10)
+    select_polynomial_degree(n_samples=1500, noise=10)
 
+    # question 7-8
     select_regularization_parameter()
